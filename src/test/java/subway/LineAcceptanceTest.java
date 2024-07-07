895c75319b38d219d0cbab5da9b35d89f6cdfc48
m5s3 <devmyoungsoo2@gmail.com>
2024-07-07 20:30:44 +0900

    test: 테스트 격리를 위해 실행전에 각 테이블에 `truncate` 쿼리 실행

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -3,21 +3,22 @@ package subway;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
-import io.restassured.mapper.ObjectMapper;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @DisplayName("지하철 노선 관련 기능")
+@Sql(scripts = {"/truncate.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
     /**
