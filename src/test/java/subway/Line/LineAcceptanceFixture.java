7e1067957093b826f70aea42e17c44e618371767
m5s3 <devmyoungsoo2@gmail.com>
2024-07-15 21:10:19 +0900

    refactor(LineAcceptanceFixture): 미사용 코드 삭제

--- src/test/java/subway/Line/LineAcceptanceFixture.java
+++ src/test/java/subway/Line/LineAcceptanceFixture.java
@@ -1,15 +1,7 @@
 package subway.Line;
 
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class LineAcceptanceFixture {
@@ -32,24 +24,6 @@ public class LineAcceptanceFixture {
         params.put("downStationId", downStationId);
         params.put("distance", 25);
         return params;
-    }
-
-    private static ExtractableResponse<Response> requestCreateLine(Map<String, Object> params) {
-        return RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/lines")
-                .then().log().all()
-                .extract();
-    }
-
-    private static ExtractableResponse<Response> requestCreateStation(String stationName) {
-        return RestAssured.given().log().all()
-                .body(createStationParams(stationName))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all()
-                .extract();
     }
 
     private static Map<String, String> createStationParams(String name) {
